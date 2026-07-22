a1065785f7c77128629dfd88d7a70c54c5e99c22
strvert <strv@yonenki.day>
2026-07-23 06:48:43 +0900

    Bindings - Export the generated driver ABI version

--- dokan-sys/src/generate_version.c
+++ dokan-sys/src/generate_version.c
@@ -5,6 +5,7 @@ int main() {
 	FILE *version_rs = fopen("version.rs", "w");
 	if (version_rs == NULL) return 1;
 	fprintf(version_rs, "pub const DOKAN_VERSION: u32 = %d;\n", DOKAN_VERSION);
+	fprintf(version_rs, "pub const DOKAN_DRIVER_VERSION: u32 = %d;\n", DOKAN_DRIVER_VERSION);
 	fprintf(version_rs, "pub const DOKAN_MINIMUM_COMPATIBLE_VERSION: u32 = %d;\n", DOKAN_MINIMUM_COMPATIBLE_VERSION);
 	fprintf(version_rs, "pub const DOKAN_DRIVER_NAME: &str = \"%ls\";\n", DOKAN_DRIVER_NAME);
 	fprintf(version_rs, "pub const DOKAN_NP_NAME: &str = \"%ls\";\n", DOKAN_NP_NAME);
